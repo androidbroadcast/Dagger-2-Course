87831d4ffbc38a176752f1b9abdaff241a0d7501
Kirill Rozov <kirill@androidbroadcast.dev>
2021-11-16 13:01:42 +0300

    Update sample

--- 02_basic/build.gradle.kts
+++ 02_basic/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("kotlin-android")
     id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -23,25 +24,29 @@ android {
     }
 }
 
-val daggerVersion = "2.36"
-val archComponents = "2.4.0-alpha02"
+val daggerVersion = "2.40"
+val archComponents = "2.4.0"
 
 dependencies {
     implementation("com.google.dagger:dagger:$daggerVersion")
     kapt("com.google.dagger:dagger-compiler:$daggerVersion")
 
+
+    implementation("com.google.dagger:hilt-android:$daggerVersion")
+    kapt("com.google.dagger:hilt-compiler:$daggerVersion")
+
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
 
-    implementation("androidx.fragment:fragment-ktx:1.3.5")
-    implementation("androidx.activity:activity-ktx:1.2.3")
-    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.fragment:fragment-ktx:1.3.6")
+    implementation("androidx.activity:activity-ktx:1.4.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$archComponents")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$archComponents")
     implementation("androidx.lifecycle:lifecycle-common-java8:$archComponents")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$archComponents")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 
     implementation("com.github.kirich1409:viewbindingpropertydelegate-noreflection:1.4.6")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,9 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta05")
+        classpath("com.android.tools.build:gradle:7.1.0-beta03")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
